b26a1dd54c3cd428f39a966b240418c1a0431c9b
Your Name <mahabagodwinanton@gmail.com>
2024-05-08 15:10:31 +0800

    user input

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,17 @@
 #pseudocode
 
 #user input choice of operation
+def calculator_app():
+    while True:
+        try:
+            print("Select operation:")
+            print("1.Add")
+            print("2.Subtract")
+            print("3.Multiply")
+            print("4.Divide")
+
+            choice = input("Enter choice(1/2/3/4): ")
+
 # input two numbers from the user
 # perform the chosen operation
 # add "do you want to try again"
